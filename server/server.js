19fd180f7ba5e25065968871c372ec8f4851c831
Steve Fokas <sf487552@gmail.com>
2021-11-13 00:43:33 -0500

    aj;fldkja;gf

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const app = express();
